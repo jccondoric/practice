49c7ef7665046e9f7054055ef262eea578ef205b
jccondoric <www.hackdcjcc@hotmail.com>
2024-05-03 00:15:48 -0400

    Ejercicio 1 de php

---
+++ PHP/tabla.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+$a = $_GET['a'];
+
+echo "<table border = '1'>";
+
+echo "<tr><td colspan = '5'>Tabla de multiplicar de $a</td></tr>";
+
+for($i=1;$i <= 25; $i++){
+
+    if(($i-1)%5==0) echo "</tr><tr>";
+
+    echo "<td> $a x $i = ".$a * $i."</td>";
+}
+
+echo "</td>";
+
+
+
+echo "</table>";
+
+
+?>
+    
+</body>
+</html>
